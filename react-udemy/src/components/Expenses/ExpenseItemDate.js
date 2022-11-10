73fb4e77f642c4aa3d124e4ac8d16ed4bb9fa63f
ManhHT <datpd@rikkeisoft.com>
2022-11-10 11:45:17 +0700

    test

--- react-udemy/src/components/Expenses/ExpenseItemDate.js
+++ react-udemy/src/components/Expenses/ExpenseItemDate.js
@@ -2,19 +2,18 @@ import React from "react";
 import "./ExpenseItemDate.css";
 import { Button } from "antd";
 const ExpenseItemDate = (props) => {
-  const now = new Date();
-  const month = now.toLocaleString('en-US', { month: 'long' });
-  const date = now.toLocaleString('en-US', { day: '2-digit' });
-  const year = now.getFullYear();
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const date = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   const clickHandler = (e) => {
     
  };
-console.log("Prop1",props);
   return (
     <div className="expense-date">
-      <div>{props.date.month}</div>
-      <div>{props.date.date}</div>
-      <div>{props.date.year}</div>
+      <div>{month}</div>
+      <div>{date}</div>
+      <div>{year}</div>
     </div>
   );
 };
